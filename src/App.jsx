10475dd5f115d6564a04c2dd302e4ca8dbc1765e
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-12-04 19:13:46 +0500

    [pakage added cors]

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import AuthenticatedRoutes from "./routes/AuthenticatedRoutes";
 import { useState } from "react";
 import MainLayout from "./components/MainLayout";
 import './App.css'
+import { WorkoutServices } from "./services/WorkoutServices";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  WorkoutServices.getWorkouts()
   return (
     <>
       <BrowserRouter>
---
+++ src/services/WorkoutServices.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const getWorkouts=async()=>{
+    try {
+        const response=await axios.get('https://fitness-tracker-backend-1-vqav.onrender.com/workout/get-workouts')
+        console.log(response.data,'data')
+    } catch (error) {
+        
+    }
+}
+getWorkouts()
+
+export const WorkoutServices={
+    getWorkouts,
+}
